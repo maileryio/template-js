9b307284431027c587eedc69d8f3f714d54504c3
Igor Maliy <imaliy.filsh@gmail.com>
2020-02-10 23:46:49 +0200

    Create index.spec.js

---
+++ test/index.spec.js
@@ -0,0 +1,11 @@
+// more example https://medium.com/js-dojo/unit-testing-in-vue-mocha-part-2-ebf1df4b8897
+import { shallowMount } from '@vue/test-utils';
+import { expect } from 'chai';
+
+describe('_____', () => {
+  beforeEach(() => {});
+
+  it('should render _____ on mount', () => {
+    expect(true).to.be.true;
+  });
+});
